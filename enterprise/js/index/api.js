41e8f845ff36413692af0cbcc285332282c3355f
user <like@admin.com>
2023-08-18 13:25:30 +0800

    style: update index css

--- enterprise/js/index/api.js
+++ enterprise/js/index/api.js
@@ -3,26 +3,33 @@ function getRecommended() {
     url: 'https://www.fastmock.site/mock/899de9c07424960e972a6ae2615eba3f/api/recommended-list',
     method: 'get'
   }).then(res => {
-    console.log(res.data)
+    // console.log(res.data)
     const news = document.getElementById('news-recommended')
     // Create the fragment
     const frag = document.createDocumentFragment();
+    // <a class="flex-1 w-full px-2" href="/enterprise-website/newsDetail?id=1" target="_blank">
+		// 		<img class="w-[348px] h-[506px] left-0 shadow" src="./images/index/news2.png">
+		// 		<div class="w-[348px] left-0 text-white text-xl font-semibold leading-9">支持触觉反馈，Manus VR推出全新Prime VR手套</div>
+		// 		<div class="w-[348px] left-0 text-white text-sm font-normal leading-7">
+		// 			第三届国际印刷工业发展论坛（Forum-PI 2013）于2013年5月13日在北京举行，主办方邀请了来自中国、美国、德国、英国、意大利、日本、澳大利亚、俄罗斯、印度、印尼、南非、巴西、
+		// 		</div>
+		// 	</a>
     for (const item of res.data.data) {
-      const div = document.createElement("div");
-      div.className = 'flex-1 relative';
+      const a = document.createElement("a");
+      a.className = 'flex-1 w-full px-2';
       const child_img = document.createElement("img");
-      child_img.className='w-[348px] h-[506px] left-0 top-[325px] absolute shadow';
+      child_img.className='w-[348px] h-[506px] left-0 shadow';
       child_img.src=item.image;
       const child_title = document.createElement("div");
-      child_title.className = 'w-[348px] left-0 top-[867px] absolute text-white text-xl font-semibold leading-9';
+      child_title.className = 'w-[348px] left-0 text-white text-xl font-semibold leading-9';
       child_title.innerText=item.title
       const child_des = document.createElement("div");
-      child_des.className='w-[348px] left-0 top-[967px] absolute text-white text-sm font-normal leading-7';
+      child_des.className='w-[348px] left-0 text-white text-sm font-normal leading-7';
       child_des.innerText=item.desc
-      div.appendChild(child_img)
-      div.appendChild(child_title)
-      div.appendChild(child_des)
-      frag.appendChild(div)
+      a.appendChild(child_img)
+      a.appendChild(child_title)
+      a.appendChild(child_des)
+      frag.appendChild(a)
     }
     news.appendChild(frag)
   })
--- qiye/js/index/index.js
+++ qiye/js/index/index.js
@@ -1,43 +1,58 @@
 // // set the default active slide to the first one
-// let slideIndex = 1;
-// showSlide(slideIndex);
+let slideIndex = 1;
+showSlide(slideIndex);
 
-// // change slide with the dots
-// function currentSlide(n) {
-//     showSlide(slideIndex = n);
-// }
+let timer = setInterval(() => {
+    slideIndex = slideIndex +1
+    showSlide(slideIndex)
+}, 5000)
 
-// function showSlide(n) {
-//     let i;
-//     const slides = document.getElementsByClassName("slide");
-//     const dots = document.getElementsByClassName('dot');
+// change slide with the dots
+function currentSlide(n) {
+    clearInterval(timer)
+    showSlide(slideIndex = n);
+    timer = setInterval(() => {
+        slideIndex = slideIndex +1
+        showSlide(slideIndex)
+    }, 5000)
+}
 
-//     if (n > slides.length) { slideIndex = 1 }
-//     if (n < 1) { slideIndex = slides.length }
+function showSlide(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName('dot');
 
-//     // hide all slides
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].classList.add('hidden');
-//     }
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
 
-//     // remove active status from all dots
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].classList.remove('border-white');
-//         dots[i].classList.add('border-opacity-50');
-//     }
+    // hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.add('hidden');
+    }
 
-//     // show the active slide
-//     slides[slideIndex - 1].classList.remove('hidden');
+    // remove active status from all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('border-white');
+        dots[i].classList.add('border-opacity-50');
+    }
 
-//     // highlight the active dot
-//     dots[slideIndex - 1].classList.remove('border-opacity-50');
-//     dots[slideIndex - 1].classList.add('border-white');
-// }
+    // show the active slide
+    slides[slideIndex - 1].classList.remove('hidden');
+
+    // highlight the active dot
+    dots[slideIndex - 1].classList.remove('border-opacity-50');
+    dots[slideIndex - 1].classList.add('border-white');
+}
 
 // 响应式导航按钮的交互
-const menuButton = document.getElementById('openMainMenu');
+const openMainMenu = document.getElementById('openMainMenu');
+const closeMainMenu = document.getElementById('closeMainMenu');
 const navList = document.getElementById('mobile-menu');
 
-menuButton.addEventListener('click', () => {
+openMainMenu.addEventListener('click', () => {
+    navList.classList.toggle('hidden');
+});
+
+closeMainMenu.addEventListener('click', () => {
     navList.classList.toggle('hidden');
 });
