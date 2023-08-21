993d9713da6b5147720ec3cc3e4b335c0d6518e1
user <like@admin.com>
2023-08-21 10:29:40 +0800

    wip: news

--- enterprise/js/news/api.js
+++ enterprise/js/news/api.js
@@ -1,5 +1,4 @@
-// https://api.lkm8.com/v1/news?pageSize=10&page=1
-function getNewsList(page=1, pageSize=8) {
+function getNewsList(page=1, pageSize=8, select=0) {
   axios({
     url: 'https://api.lkm8.com/v1/news',
     method: 'get',
@@ -8,8 +7,8 @@ function getNewsList(page=1, pageSize=8) {
       pageSize
     }
   }).then(res => {
-    console.log(res.data)
-    const news = document.getElementById('news-list')
+    console.log(res.data);
+    const news = document.getElementById('news-list');
     // Create the fragment
     const frag = document.createDocumentFragment();
     for (const item of res.data.data.list) {
@@ -20,20 +19,32 @@ function getNewsList(page=1, pageSize=8) {
       child_img.className='w-full h-full';
       child_img.src=item.image;
       const child_div = document.createElement("div");
-      child_div.className='relative'
-      child_div.appendChild(child_img)
+      child_div.className='relative';
+      child_div.appendChild(child_img);
       const child_title = document.createElement("div");
       child_title.className = 'text-black text-xl font-semibold mt-6 md:mt-9 line-clamp-2';
-      child_title.innerText=item.title
+      child_title.innerText=item.title;
       const child_des = document.createElement("div");
       child_des.className='text-black text-sm font-normal mt-6 line-clamp-4';
-      child_des.innerText=item.summary
-      a.appendChild(child_div)
-      a.appendChild(child_title)
-      a.appendChild(child_des)
-      frag.appendChild(a)
+      child_des.innerText=item.summary;
+      a.appendChild(child_div);
+      a.appendChild(child_title);
+      a.appendChild(child_des);
+      frag.appendChild(a);
     }
-    news.appendChild(frag)
+    news.replaceChildren(frag);
+    if (select) {
+      history.pushState({
+        page,
+        pageSize
+      }, 'history', location.href)
+    }
   })
+}
+function pageSizeChange() {
+  const pageSize = document.getElementById('pageSizeSelect');
+  const page = new URLSearchParams(window.location.search).get('page')
+  console.log(pageSize.value)
+  getNewsList(page, pageSize.value, 1)
 }
 getNewsList()
